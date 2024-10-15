87f42ed4963423b6eb023f44af3e92d8c9c04908
Mahirs7 <mahir.s.sabharwal@gmail.com>
2024-10-15 01:20:29 -0500

    streetbytes

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -8,9 +8,9 @@ interface Project {
 const projectsData: Project[] = [
   {
     title: 'StreetByte',
-    description: `A mobile app that allows students on university campus towns to locate food trucks in real time `,
+    description: `A mobile app that allows students on university campus towns to locate food trucks in real time`,
     imgSrc: '/static/images/google.png',
-    href: 'https://www.google.com',
+    href: 'https://mahirsabharwal.vercel.app/blog/streetbyte',
   },
   {
     title: 'MassProd',
@@ -18,12 +18,12 @@ const projectsData: Project[] = [
     imgSrc: '/static/images/google.png',
     href: 'https://www.google.com',
   },
-  {
-    title: 'MassProd',
-    description: `A SaaS product for TV production companies to efficiently manage projects, organize tasks, and facilitate internal and external communication`,
-    imgSrc: '/static/images/google.png',
-    href: 'https://www.google.com',
-  },
+  // {
+  //   title: 'MassProd',
+  //   description: `A SaaS product for TV production companies to efficiently manage projects, organize tasks, and facilitate internal and external communication`,
+  //   imgSrc: '/static/images/google.png',
+  //   href: 'https://www.google.com',
+  // },
 ]
 
 export default projectsData
